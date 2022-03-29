0fc30d9f40a34de2f04b053836a85dcc80b377f4
Christopher Xu <cyx3@illinois.edu>
2022-03-29 00:16:40 -0500

    plot of footpos

--- notebooks/animation.py
+++ notebooks/animation.py
@@ -1,99 +1,162 @@
-"""
-===========================
-The double pendulum problem
-===========================
 
-This animation illustrates the double pendulum problem.
-"""
-
-# Double pendulum formula translated from the C code at
-# http://www.physics.usyd.edu.au/~wheat/dpend_html/solve_dpend.c
-
-from numpy import sin, cos
+from numpy import sin, cos, radians
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.integrate as integrate
+from scipy.optimize import fsolve 
 import matplotlib.animation as animation
 
 
+#known values
+L1 = 9
+L2 = 2.2
+L3 = 4
+L4 = 6
+L5 = 1
+L6 = 6
+L7 = 10
+L8 = 1
+L9 = 9
+L10 = 1
+L11 = 6
+Lx = 6.814
+theta1 = radians(-90)
+theta2 = radians(-105.8)
+thetaA = radians(10)
+thetaB = radians(2)
+thetaC = radians(2)
+thetaD = radians(2)
 
-G = 9.8  # acceleration due to gravity, in m/s^2
-L1 = 1.0  # length of pendulum 1 in m
-L2 = 1.0  # length of pendulum 2 in m
-M1 = 1.0  # mass of pendulum 1 in kg
-M2 = 1.0  # mass of pendulum 2 in kg
+sample_guess = [
+    radians(-114.7), #thetaX
+    radians(-44.6), #theta3
+    radians(-34.6), #theta4
+    radians(-36.6), #theta5
+    radians(-23.8), #theta6
+    radians(-149.9), #theta7
+    radians(-148.1), #theta8
+    radians(-150.1), #theta9
+    radians(-36.6), #theta10
+    radians(-34.6), #theta11
+]
+
+def solveLegPos(guess, doPlot=False):
+
+    def func(angles):
+
+        [thetaX, theta3, theta4, theta5, theta6, theta7, theta8, theta9, theta10, theta11] = angles
+
+        return [
+            Lx*cos(thetaX) + L3*cos(theta3) - L1*cos(theta1),
+            Lx*sin(thetaX) + L3*sin(theta3) - L1*sin(theta1),
+
+            L4*cos(theta4) + L5*cos(theta5) - L6*cos(theta6) - L2*cos(theta2) + L8*cos(theta8),
+            L4*sin(theta4) + L5*sin(theta5) - L6*sin(theta6) - L2*sin(theta2) + L8*sin(theta8),
+
+            L5*cos(theta5) + L8*cos(theta8) + L9*cos(theta9) - L7*cos(theta7) - L10*cos(theta10),
+            L5*sin(theta5) + L8*sin(theta8) + L9*sin(theta9) - L7*sin(theta7) - L10*sin(theta10),
+            
+            theta4 - theta3 - thetaA,
+            theta5 - theta4 + thetaB,
+            theta9 - theta8 + thetaC,
+            theta11 - theta10 - thetaD
+        ]
+
+    root = fsolve(func, guess)
+
+    if doPlot:
+        print("error:",np.linalg.norm(func(root)))
+
+        [thetaX, theta3, theta4, theta5, theta6, theta7, theta8, theta9, theta10, theta11] = root
+
+        origin = np.zeros(2)
+        vecX = np.array([Lx*cos(thetaX), Lx*sin(thetaX)])
+        vec1 = np.array([L1*cos(theta1), L1*sin(theta1)])
+        vec2 = np.array([L2*cos(theta2), L2*sin(theta2)])
+        vec3 = np.array([L3*cos(theta3), L3*sin(theta3)])
+        vec4 = np.array([L4*cos(theta4), L4*sin(theta4)])
+        vec5 = np.array([L5*cos(theta5), L5*sin(theta5)])
+        vec6 = np.array([L6*cos(theta6), L6*sin(theta6)])
+        vec7 = np.array([L7*cos(theta7), L7*sin(theta7)])
+        vec8 = np.array([L8*cos(theta8), L8*sin(theta8)])
+        vec9 = np.array([L9*cos(theta9), L9*sin(theta9)])
+        vec10 = np.array([L10*cos(theta10), L10*sin(theta10)])
+        vec11 = np.array([L11*cos(theta11), L11*sin(theta11)])
+
+        #[x0, y0, x1, y1]
+        linkX = np.array([origin, vecX])
+        link1 = np.array([origin, vec1])
+        link2 = np.array([link1[1], link1[1] + vec2])
+        link3 = np.array([linkX[1], linkX[1] + vec3])
+        link4 = np.array([link1[1], link1[1] + vec4])
+        link5 = np.array([link4[1], link4[1] + vec5])
+        link6 = np.array([link2[1], link2[1] + vec6])
+        link7 = np.array([link4[1], link4[1] + vec7])
+        link8 = np.array([link5[1], link5[1] + vec8])
+        link9 = np.array([link8[1], link8[1] + vec9])
+        link10 = np.array([link7[1], link7[1] + vec10])
+        link11 = np.array([link10[1], link10[1] + vec11])
+
+        links = np.array([linkX, link1, link2, link3, link4, link5,
+                        link6, link7, link8, link9, link10, link11])
+
+        fig, ax = plt.subplots(figsize=(4, 4))
+        ax.axis([-16, 16, -30, 2])
+        plt.quiver(links[:, 0, 0], links[:, 0, 1], links[:, 1, 0]-links[:, 0, 0], links[:, 1, 1] - links[:, 0, 1],
+                angles='xy', scale_units='xy', scale=1, width=0.005, headwidth=2)
+
+    #return position of foot
+    vec1 = np.array([L1*cos(theta1), L1*sin(theta1)])
+    vec4 = np.array([L4*cos(theta4), L4*sin(theta4)])
+    vec7 = np.array([L7*cos(theta7), L7*sin(theta7)])
+    vec10 = np.array([L10*cos(theta10), L10*sin(theta10)])
+    vec11 = np.array([L11*cos(theta11), L11*sin(theta11)])
+
+    footpos = vec1 + vec4 + vec7 + vec10 + vec11
+
+    return footpos
 
 
-def derivs(state, t):
 
-    dydx = np.zeros_like(state)
-    dydx[0] = state[1]
+import itertools
 
-    del_ = state[2] - state[0]
-    den1 = (M1 + M2)*L1 - M2*L1*cos(del_)*cos(del_)
-    dydx[1] = (M2*L1*state[1]*state[1]*sin(del_)*cos(del_) +
-               M2*G*sin(state[2])*cos(del_) +
-               M2*L2*state[3]*state[3]*sin(del_) -
-               (M1 + M2)*G*sin(state[0]))/den1
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-    dydx[2] = state[3]
 
-    den2 = (L2/L1)*den1
-    dydx[3] = (-M2*L2*state[3]*state[3]*sin(del_)*cos(del_) +
-               (M1 + M2)*G*sin(state[0])*cos(del_) -
-               (M1 + M2)*L1*state[1]*state[1]*sin(del_) -
-               (M1 + M2)*G*sin(state[2]))/den2
-
-    return dydx
-
-# create a time array from 0..100 sampled at 0.05 second steps
-dt = 0.05
-t = np.arange(0.0, 20, dt)
-
-# th1 and th2 are the initial angles (degrees)
-# w10 and w20 are the initial angular velocities (degrees per second)
-th1 = 120.0
-w1 = 0.0
-th2 = -10.0
-w2 = 0.0
-
-# initial state
-state = np.radians([th1, w1, th2, w2])
-
-# integrate your ODE using scipy.integrate.
-y = integrate.odeint(derivs, state, t)
-
-x1 = L1*sin(y[:, 0])
-y1 = -L1*cos(y[:, 0])
-
-x2 = L2*sin(y[:, 2]) + x1
-y2 = -L2*cos(y[:, 2]) + y1
-
-fig = plt.figure()
-ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
-ax.grid()
-
-line, = ax.plot([], [], 'o-', lw=2)
-time_template = 'time = %.1fs'
-time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+def data_gen():
+    for cnt in itertools.count():
+        t = cnt / 10
+        yield t, np.sin(2*np.pi*t) * np.exp(-t/10.)
 
 
 def init():
-    line.set_data([], [])
-    time_text.set_text('')
-    return line, time_text
+    ax.set_ylim(-1.1, 1.1)
+    ax.set_xlim(0, 10)
+    del xdata[:]
+    del ydata[:]
+    line.set_data(xdata, ydata)
+    return line,
+
+fig, ax = plt.subplots()
+line, = ax.plot([], [], lw=2)
+ax.grid()
+xdata, ydata = [], []
 
 
-def animate(i):
-    thisx = [0, x1[i], x2[i]]
-    thisy = [0, y1[i], y2[i]]
+def run(data):
+    # update the data
+    t, y = data
+    xdata.append(t)
+    ydata.append(y)
+    xmin, xmax = ax.get_xlim()
 
-    line.set_data(thisx, thisy)
-    time_text.set_text(time_template % (i*dt))
-    return line, time_text
+    if t >= xmax:
+        ax.set_xlim(xmin, 2*xmax)
+        ax.figure.canvas.draw()
+    line.set_data(xdata, ydata)
 
-ani = animation.FuncAnimation(fig, animate, np.arange(1, len(y)),
-                              interval=25, blit=True, init_func=init)
+    return line,
 
-# ani.save('double_pendulum.mp4', fps=15)
+ani = animation.FuncAnimation(fig, run, data_gen, interval=10, init_func=init)
 plt.show()
